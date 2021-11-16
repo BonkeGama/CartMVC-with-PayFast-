b7bad848c299daddfd62d71a968239614e2e259b
Bonke Gama <Bonkeg18@gmail.com>
2021-11-16 11:17:19 +0200

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CartMvc.MvcApplication" Language="C#" %>
---
+++ Models/Shop.Logic/Customer_Business.cs
@@ -0,0 +1,53 @@
+﻿using CartMvc.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CartMvc.Shop.Logic
+{
+    public class Customer_Business
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public List<Patient> all()
+        {
+            return db.Patients.ToList();
+        }
+        public bool add(Patient model)
+        {
+            try
+            {
+                db.Patients.Add(model);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            { return false; }
+        }
+        public bool edit(Patient model)
+        {
+            try
+            {
+                db.Entry(model).State = EntityState.Modified;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            { return false; }
+        }
+        public Patient find_by_id(int? id)
+        {
+            return db.Patients.Find(id);
+        }
+
+        public string getGender(string id_num)
+        {
+            if (Convert.ToInt16(id_num.Substring(7, 1)) >= 5)
+                return "Male";
+            else
+                return "Female";
+        }
+    }
+}
